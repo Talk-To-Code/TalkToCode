1371d8b0f771e329bfd002de266c4feb852ca31b
LawMan <lawrenciok@gmail.com>
2019-10-14 21:52:29 +0800

    testing import function. does not seem to work.

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { print } from 'util';
+import {correct} from './lawman_words.js';
 const {spawn} = require('child_process');
 
 // this method is called when your extension is activated
@@ -19,35 +21,41 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('coding by dictation!');
+		console.log(word_corrector.correct("hello"));
 		listen();
 	});
 
 	context.subscriptions.push(disposable);
 }
 
+function get_structured_command(speech: string) {
+	
+}
+
 
 function listen() {
 
 	// cwd is the current working directory. Make sure you update this.
+	
 	let cwd = 'C:\\Users\\Lawrence\\Desktop\\talktocode\\talk-to-code\\src';
 	// cred is the credential json from google you have to obtain to use their speech engine.
 	let cred = 'C:\\Users\\Lawrence\\Desktop\\fyp\\benchmarking\\test_google_cloud\\My-Project-f25f37c6fac1.json';
 	
 	const child = spawn('node', ['speech_recognizer.js'], {shell:true, cwd: cwd, env: {GOOGLE_APPLICATION_CREDENTIALS: cred}});
-	child.stdout.on('data', (data)=>{
+	child.stdout.on('data', (data: string)=>{
 		let transcribed_word = data.toString()
-		console.log(transcribed_word)
+		// console.log(transcribed_word)
 		if (transcribed_word == 'Listening\n') {
 			vscode.window.showInformationMessage('Begin Speaking!');
 		}
 		else {
-			write_to_cursor(data.toString());
+			get_structured_command(data.toString())
 		}
 	});
 
 }
 
-function write_to_cursor(text) {
+function write_to_cursor(text: string) {
 	let editor = vscode.window.activeTextEditor;
 	if (editor) {
 		let cursor_position = editor.selection.anchor;
