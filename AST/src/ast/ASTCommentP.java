b3bdc09e19e263a3fc54c0c696c11acb4914aee3
Soon Wei Jun <e0032329@u.nus.edu>
2019-12-22 15:19:44 +0800

    Added Comment class and the respective child classes.

---
+++ AST/src/ast/ASTCommentP.java
@@ -0,0 +1,31 @@
+package ast;
+/**
+ * @author SOON WEI JUN A0156192A
+ * this class is mainly in charge of syntax generation of comments in Python Programs
+ */
+public class ASTCommentP extends ASTComment {
+
+	public ASTCommentP() {
+		super();
+	}
+
+	public String toSyntax() {
+		for(int i = 0; i < this.statements.size(); i++) {
+			this.result = "";
+			if(this.statements.get(i) instanceof ASTSimpleStatement) {
+				this.result += "#";
+				if(this.statements.get(i).toSyntax().charAt(0) != '\"') this.result += this.statements.get(i).toSyntax();
+				else this.result += this.statements.get(i).toSyntax().substring(1, this.statements.get(i).toSyntax().length()-3);
+			}
+			else {
+				this.result += "\'\'\'\n";
+				for(int j = 0; j < this.indent; j++) this.result+="\t";
+				this.result += this.statements.get(i).toSyntax();
+				for(int j = 0; j < this.indent; j++) this.result+="\t";
+				this.result += "\'\'\'\n";
+			}
+		}
+		
+		return this.result;
+	}
+}
