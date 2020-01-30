4905480e99f0447a5a0074d1a3fb2332ce23efb9
LawMan <lawrenciok@gmail.com>
2020-01-30 13:20:21 +0800

    updated test cases

--- src/tester.ts
+++ src/tester.ts
@@ -4,14 +4,14 @@ export function runTestCases() {
 
     var test_cases = [[""], [""]];
 
-    test_cases = generate_test_cases("declare_assign");
-    runTestCase(test_cases[0], test_cases[1]);
+    // test_cases = generate_test_cases("declare_assign");
+    // runTestCase(test_cases[0], test_cases[1]);
 
     test_cases = generate_test_cases("if_block");
     runTestCase(test_cases[0], test_cases[1]);
 
-    test_cases = generate_test_cases("for_loop");
-    runTestCase(test_cases[0], test_cases[1]);
+    // test_cases = generate_test_cases("for_loop");
+    // runTestCase(test_cases[0], test_cases[1]);
 
 }
 
@@ -35,26 +35,27 @@ function generate_test_cases(cases: string) {
     }
 
     else if (cases == "if_block") {
-        test_cases[0] = ["begin if hello greater than 5", "declare integer i equal 4", "i equals 6"];
+        test_cases[0] = ["begin if hello greater than 5", "declare integer i equal 4", "i equals 6",
+        "begin if", "i", "equals", "4", "declare count equal 6"];
 
-        test_cases[1] = ["if #condition #variable hello > #value 5  #if_branch_start",
+        test_cases[1] = ["if #condition #variable hello > #value 5 #if_branch_start",
         "#create int #variable i #value 4 #dec_end;;","#assign #variable i #with #value 6;;",
-        "","#if_branch_end;;"];
+        "if #condition #variable i == #value 4  #if_branch_start","declare count equal 6","#if_branch_end;;",
+        "#if_branch_end;;"];
     }
-    else if (cases == "for_loop")
+    else if (cases == "for_loop") {
         test_cases[0] = ["begin Loop condition I equal 0 condition I less than 5 condition I plus plus",
         "declare integer hello equals 5"];
-
         test_cases[1] = ["for #condition #assign #variable I #with #value 0 #condition #variable I < #value 5 #condition #post #variable I ++ #for_start",
         "#create int #variable hello #value 5 #dec_end;;","","#for_end;;"];
-
+    }
+    
     return test_cases
 }
 
 /* Run and compare my output and correct output using test cases. */
 function runTestCase(test_cases: string[], correct_output: string[]) {
     var test_manager = new StructCommandManager();
-
     var i;
     for (i = 0; i < test_cases.length; i++) {
         test_manager.parse_speech(test_cases[i]);
