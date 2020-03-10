11f8967d57cc35e5f229febc099fb059323388e5
Soon Wei Jun <e0032329@u.nus.edu>
2020-03-10 18:03:26 +0800

    Added support and test cases for C pointers. Updated garammarRule.txt

---
+++ AST/src/ast/ASTExpressionUnitPointer.java
@@ -0,0 +1,28 @@
+package ast;
+/**
+ * @author SOON WEI JUN A0156192A
+ * This class is mainly in charge of construction of AST nodes that represents
+ * a pointer 
+ * and syntax generation of the respective term.
+ * This is available in C only.
+ */
+public class ASTExpressionUnitPointer extends ASTExpressionUnit{
+	private static final String NODE_TYPE = "Pointer";
+	private ASTExpressionUnit name;
+	public ASTExpressionUnitPointer() {
+		super();
+	}
+
+	public ASTExpressionUnitPointer(ASTExpressionUnit name){
+		this();
+		this.name = name;
+	}
+	public String toSyntax(){
+		this.result = "*" + this.name.toSyntax();
+		if(this.isQuoted) quote();
+		return super.toSyntax();
+	}
+	public String typeof() {
+		return super.typeof()+"->"+NODE_TYPE;
+	}
+}
